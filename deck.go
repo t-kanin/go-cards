f4d0eec0109d8df9a1c68f3d7648a2f6d897046a
Kanin Tansirisittikul <tan.kanin98@gmail.com>
2022-04-10 00:39:55 +0700

    init proj. with new deck

---
+++ deck.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// create type deck
+// which is slice of strings
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
