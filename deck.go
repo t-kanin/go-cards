03603081afe4198e48cf4d1ee8c07b2302d9ad31
Kanin Tansirisittikul <tan.kanin98@gmail.com>
2022-04-10 20:08:46 +0700

    read and write cards from file

--- deck.go
+++ deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 // create type deck
 // which is slice of strings
@@ -26,4 +31,30 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, size int) (deck, deck) {
+	return d[:size], d[size:]
+}
+
+func (d deck) toString() string {
+	// this will return slices of string
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	byteString := []byte(d.toString())
+	// 0666 means anyone can read and write this file
+	return ioutil.WriteFile(filename, byteString, 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
 func main() {
-	cards := newDeck()
+	cards := newDeckFromFile("cards.txt")
 	cards.print()
 }
